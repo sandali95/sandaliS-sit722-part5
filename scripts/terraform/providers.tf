7b7d384511af938217a73104ab34395cba915306
Sandali Samarawickrama <wathmali123@gmail.com>
2024-09-13 17:55:49 +1000

    add client secret as a variable

--- scripts/terraform/providers.tf
+++ scripts/terraform/providers.tf
@@ -1,32 +1,16 @@
 # Initialises Terraform providers and sets their version numbers.
 
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "~> 3.71.0"
-#     }
-#   }
-
-#   required_version = ">= 1.5.6"
-# }
-
-# provider "azurerm" {
-#   features {}
-# }
-
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.x"
+      version = "~> 3.71.0"
     }
   }
+
+  required_version = ">= 1.5.6"
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
